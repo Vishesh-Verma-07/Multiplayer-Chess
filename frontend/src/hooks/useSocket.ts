f1a81091d15d7a73842dbcb6c7dc9dc8b5d857bf
Vishesh Verma <visheshverma250703@gmail.com>
2026-04-22 10:48:15 +0530

    altered the backend url import

--- frontend/src/hooks/useSocket.ts
+++ frontend/src/hooks/useSocket.ts
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react"
 
 
-const WS_URL = import.meta.env.ENV == "production" ? import.meta.env.VITE_WS_URL : "ws://localhost:8080/ws";
+const WS_URL = import.meta.env.ENV == "production" ? import.meta.env.VITE_WEBSOCKET_BACKEND_URL : "ws://localhost:8080/ws";
 
 export const useSocket = () => {
     const [socket, setSocket] = useState<WebSocket | null>(null);
 
     useEffect(() => {
-        console.log("trying to connect to the url");
+        console.log("trying to connect to the url", WS_URL);
         const ws = new WebSocket(WS_URL);
         console.log("socket created is ", ws)
 

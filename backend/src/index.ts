71dd9c7b44f7f4b07396e172ae2db3890f6f16bb
Vishesh Verma <visheshverma250703@gmail.com>
2026-05-19 00:47:23 +0530

    added the proper port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,13 +4,13 @@ import { GameManager } from "./models/GameManager";
 import { handleHttpRequest } from "./routes/httpRoutes";
 import { registerWebSocketRoutes } from "./routes/wsRoutes";
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 const server = createServer(handleHttpRequest);
 
 const gameManager = new GameManager();
 
-registerWebSocketRoutes(server, { port: PORT, gameManager });
+registerWebSocketRoutes(server, { port: Number(PORT), gameManager });
 
 server.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
